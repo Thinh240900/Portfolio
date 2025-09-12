5f5ef203241f18ec39ce72a6efeb9c3ed6a45bfc
ThinhBanh <thinhbanhphuc@gmail.com>
2025-09-13 00:23:43 +0700

    A complete but basic, static resume

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -13,7 +13,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from os import getenv
 
+from dotenv import load_dotenv
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -21,25 +25,26 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-REMOVED = getenv('REMOVED')
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv('IS_DEBUG', True)
 
 ALLOWED_HOSTS = [
-    getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1')
+    getenv('ALLOWED_HOSTS', '127.0.0.1')
 ]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'basePortfolio',
+    'basePortfolio.apps.BaseportfolioConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,9 +123,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = BASE_DIR / 'uploads'
+MEDIA_URL = "/files/"
--- basePortfolio/views.py
+++ basePortfolio/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+
 
 # Create your views here.
+
+
+class HomePageView(View):
+    template_name = 'basePortfolio/home.html'
+    def get(self, request):
+        return render(request, self.template_name)
